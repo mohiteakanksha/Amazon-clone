12e34e0004fabf7a281118d1b7c278e44d61431e
mohiteakanksha <143405012+mohiteakanksha@users.noreply.github.com>
2023-08-28 18:06:32 +0530

    Initial commit

---
+++ todayDeal.js
@@ -0,0 +1,114 @@
+export const todayDeal = [
+    {
+        img : "box41.jpg",
+        discount : 24,
+        DealOfDay : "Great Freedom Sale",
+        desc : "Samsung Galaxcy M34"
+    },
+    {
+        img : "box42.jpg",
+        discount : 45,
+        DealOfDay : "Great Freedom Sale",
+        desc : "Redmi 12C (Royal Blue, 4GB RAM, 64GB Storage)"
+    },
+    {
+        img : "box43.jpg",
+        discount : 76,
+        DealOfDay : "Great Freedom Sale",
+        desc : "OnePlus Nord CE 3 5G (Aqua Surge, 8GB RAM, 128GB Storage)"
+    },
+    {
+        img : "box44.jpg",
+        discount : 76,
+        DealOfDay : "Great Freedom Sale",
+        desc : "Top headsets from Oneplus, Samsung,Redmi, Realme..."
+    },
+
+    {
+        img : "box45.jpg",
+        discount : 55,
+        DealOfDay : "Great Freedom Sale",
+        desc : "JBL Audio Freedom Sale Live"
+    },
+    {
+        img : "box46.jpg",
+        discount : 58,
+        DealOfDay : "Great Freedom Sale",
+        desc : "Alexa Devices - Echo and Fire TV"
+    },
+
+    {
+        img : "box47.jpg",
+        discount : 61,
+        DealOfDay : "Great Freedom Sale",
+        desc : "Lowest Ever Price on PS5 Console"
+    },
+    {
+        img : "box48.jpg",
+        discount : 15,
+        DealOfDay : "Great Freedom Sale",
+        desc : "Realme Narzo N53 || Bye Now"
+    },
+
+    {
+        img : "box49.jpg",
+        discount : 76,
+        DealOfDay : "Great Freedom Sale",
+        desc : "Best Selling Massagers"
+    },
+    {
+        img : "box50.jpg",
+        discount : 60,
+        DealOfDay : "Great Freedom Sale",
+        desc : "Top Offers on Mice & Keyboards"
+    },
+
+    /*{
+        img : "https://m.media-amazon.com/images/I/41yxd6A+ZAL._AC_SY200_.jpg",
+        discount : 65,
+        DealOfDay : "Great Freedom Sale",
+        desc : "Top deals on Truke, Govo and more"
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/41wcMkODJLL._AC_SY200_.jpg",
+        discount : 75,
+        DealOfDay : "Great Freedom Sale",
+        desc : "Amazing deals on pTron, Jabra, Portronics"
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/61QQtY6qtHL._AC_SY200_.jpg",
+        discount : 82,
+        DealOfDay : "Great Freedom Sale",
+        desc : "Levi's, Allen Solly, ANNI DESIGNER, Janasya & more"
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/31aTPRvXiSL._AC_SY200_.jpg",
+        discount : 77,
+        DealOfDay : "Great Freedom Sale",
+        desc : "Exciting  deals on Mivi, Wecool, Wings"
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/21wNUazPKRL._AC_SY200_.jpg",
+        discount : 20,
+        DealOfDay : "Great Freedom Sale",
+        desc : "Never before deals on HP printers"
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/41bFnhrsyOL._AC_SY200_.jpg",
+        discount : 19,
+        DealOfDay : "Great Freedom Sale",
+        desc : "Vivo Y75 - 18W fast charge, FHD plus display"
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/41wyFlObIrL._AC_SY200_.jpg",
+        discount : "25",
+        DealOfDay : "Great Freedom Sale",
+        desc : "Nokia 5710 - newly launched 4G feature phone"
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/31Zq-alRH9L._AC_SY200_.jpg",
+        discount : 32,
+        DealOfDay : "Great Freedom Sale",
+        desc : "Vacuum Cleaners from Top Brands"
+    },*/
+]
